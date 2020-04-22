816e092c46a93a41a2322d2c57066b460804c1c9
sl0th <51172348+sloth-squad@users.noreply.github.com>
2020-04-22 23:57:21 +0100

    Slothbot 1.1
    - Beta Lua interpreter
    - Lacking permissions error
    - Updated .help

--- SlothBot.lua
+++ SlothBot.lua
@@ -4,15 +4,59 @@
 ]]--
 
 local Discordia = require("discordia")
-local Client = discordia.Client()
+local Client = Discordia.Client()
+local PrettyPrint = require('pretty-print')
 
 local BotToken = "Put your own token here"
 local BotFunctions = {}
-
+local Sandbox = setmetatable({ }, { __index = _G })
 local GifCount
 local ImageCount
 
 local SlothFacts = {}
+
+local function code(str)
+     return string.format('```\n%s```', str)
+ end
+ 
+ local function println(...)
+     local ret = {}
+     for i = 1, select('#', ...) do
+         local arg = tostring(select(i, ...))
+         table.insert(ret, arg)
+     end
+     return table.concat(ret, '\t')
+ end
+ 
+ local function prettyln(...)
+     local ret = {}
+     for i = 1, select('#', ...) do
+         local arg = PrettyPrint.strip(PrettyPrint.dump(select(i, ...)))
+         table.insert(ret, arg)
+     end
+     return table.concat(ret, '\t')
+ end
+ 
+ local function Interpreter(arg, msg)
+     arg = arg:gsub('```\n?', '')
+     local lines = {}
+     Sandbox.message = msg
+     Sandbox.print = function(...)
+         table.insert(lines, println(...))
+     end
+     Sandbox.p = function(...)
+         table.insert(lines, prettyln(...))
+     end
+     local fn, syntaxError = load(arg, 'DiscordBot', 't', Sandbox)
+     if not fn then return msg:reply(code(syntaxError)) end
+     local success, runtimeError = pcall(fn)
+     if not success then return msg:reply(code(runtimeError)) end
+     lines = table.concat(lines, '\n')
+     if #lines > 1990 then
+         lines = lines:sub(1, 1990)
+     end
+     return msg:reply(code(lines))
+ end
 
 local function Split(str, pat)
      local t = {} 
@@ -48,20 +92,20 @@ ImageCount = GetDirCount("Images",".png")
 
 BotFunctions[".slothfact"] = function(message)
      local Channel = message.channel
-     Channel:send(SlothFacts[math.random(#SlothFacts)])
+     Channel:send("Fact! "..SlothFacts[math.random(#SlothFacts)])
 end
 BotFunctions[".slothpic"] = function(message)
      local Channel = message.channel
-     Channel:send({file="Images/"..tostring(math.random(ImageCount))..".png"})
+     Channel:send({content="Picture!",file="Images/"..tostring(math.random(ImageCount))..".png"})
 end
 BotFunctions[".slothgif"] = function(message)
      local Channel = message.channel
-     Channel:send({file="GIF/"..tostring(math.random(GifCount))..".gif"})
+     Channel:send({content="GIF!",file="GIF/"..tostring(math.random(GifCount))..".gif"})
 end
 BotFunctions[".help"] = function(message)
      local Channel = message.channel
      local Author = message.author 
-     Channel:send{
+     Author:send{
           embed = {
                title = "Help",
                description = "Here are a list of commands",
@@ -72,10 +116,11 @@ BotFunctions[".help"] = function(message)
                fields = { 
                     {name = ".slothpic",value = "Will reply with a random picture of a Sloth",inline = true},
                     {name = ".slothgif",value = "Will reply with a random GIF of a Sloth",inline = false},
-                    {name = ".slothfact",value = "Will reply with a random fact about Sloths",inline = false}
+                    {name = ".slothfact",value = "Will reply with a random fact about Sloths",inline = false},
+                    {name = ".lua [CODE]",value = "Without the [] the bot will attempt to interpret the code you provided in lua and print the output",inline = false}
                },
                footer = {
-                    text = "SlothBot, for people passionate about sloths :)"
+                    text = "SlothBot, for people passionate about sloths :) This bot is also open-source if you wanna check it out head to https://www.github.com/sloth-squad/SlothBot"
                },
                color = 0x000000
      }}
@@ -86,11 +131,24 @@ Client:on("ready", function()
 end)
 
 Client:on('messageCreate', function(message)
+     if message.guild == nil then return end
      local Message = message.content:lower()
      local Channel = message.channel
-
+     
+     local HasAttachPermissions = message.guild.me:hasPermission(Channel,0x00008000)
+     
+     if Message:lower():sub(1,4) == ".lua" then
+          if string.find(Message,"while true do") then message:reply("I cannot compile that") end
+          Interpreter(Message:sub(6,#Message),message)
+     end
+     
      if BotFunctions[Message] ~= nil then
-          BotFunctions[Message](message)
+          if HasAttachPermissions then
+               BotFunctions[Message](message)
+          elseif not HasAttachPermissions then
+               Channel:send("I do not have permission to attach links :( sloth sad now")
+          end
+          return
      end
 end)
 
